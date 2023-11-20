855e1b98ed4bfad491173ce1c00f5e790674597f
DN1987 <147844233+DN1987@users.noreply.github.com>
2023-11-20 18:57:34 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+let email =  prompt("Email adresinizi daxil edin!");
+let password = prompt("Passwordu daxil edin!");
+
+if(email === "admin@gmail.com" && password === "12345"){
+    alert("Dogrudur");
+}
+else{
+    alert("Yanlisdir!");
+}
+
+
+
+
+
