ebbf9f9610077609688fe9a1c0a72913787de106
DN1987 <147844233+DN1987@users.noreply.github.com>
2023-11-22 19:15:19 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,42 @@
 
-let email =  prompt("Email adresinizi daxil edin!");
-let password = prompt("Passwordu daxil edin!");
+// let email =  prompt("Email adresinizi daxil edin!");
+// let password = prompt("Passwordu daxil edin!");
 
-if(email === "admin@gmail.com" && password === "12345"){
-    alert("Dogrudur");
+// if(email === "admin@gmail.com" && password === "12345"){
+//     alert("Dogrudur");
+// }
+// else{
+//     alert("Yanlisdir!");
+// }
+
+
+// function Add() {
+//     let email = document.getElementById("email").value;
+//     let password = document.getElementById("password").value;
+//     if(email === "admin@gmail.com" && password === "12345"){
+//         alert("Dogrudur");
+//     }
+//     else{
+//         document.getElementById("error").innerHTML = "Yanlisdir!"
+//     }
+// }
+
+let start = prompt(" start Reqemi daxil edin")
+let end = prompt(" end Reqemi daxil edin")
+
+if (start < end) {
+    while (start <= end) {
+        if (start % 2 == 0) {
+            console.log(start);
+        }
+        
+        start++;
+
+    }
 }
-else{
-    alert("Yanlisdir!");
+else {
+    alert("reqem secilmis araliqda deyildir!")
 }
-
-
 
 
 
